21f69ffc4f862b9085bbad99ba3cfa9c434c8bdb
Marcus Grant <marcus.grant@patternbuffer.io>
2017-09-21 22:40:00 -0400

    install script now properly check & links to dot path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # 2017 - 09 - 20
 # marcus.grant@patternbuffer.io
 # patternbuffer.io
-# 
+#
 # A script that uses git & bash to clone all the appropriate repos & sets
 # soft links to the correct places for the system to redirect to these dotfiles
 ################################################################################
@@ -14,7 +14,40 @@
 function main() {
 	# paths
 	dots_path=$(config-path)
-	link_path=$HOME/.config/vimfiles #TODO: Change to XDG_BASE_CONFIG
+	link_path=$HOME/.config/nvim #TODO: Change to XDG_BASE_CONFIG
+
+	# opening message
+	echo
+	echo "Preparing local environment for neoVim..."
+	echo "Starting by linking $link_path to"
+	echo "dotfiles in $dots_path."
+
+	# check for existence of previous paths
+	if $(is-file $link_path); then
+		echo
+		echo "File already exists in $link_path, removing it."
+		rm $link_path
+	elif $(is-link $link_path); then
+		echo
+		echo "Link already exists in $link_path, removing it."
+		rm $link_path
+	elif is-dir $link_path; then
+		echo
+		echo "Directory already exists in $link_path"
+		echo "Copying directory as $link_path.bak, and deleting original"
+		mv $link_path $link_path.bak
+	fi
+
+	# link dotfile directory for neovim to $link_path
+	ln -s $dots_path $link_path
+
+	# TODO: Other stuff
+
+
+	# complete
+	echo
+	echo "Done preparing neoVim for local environment!"
+	echo
 }
 
 function config-path() {
@@ -30,5 +63,30 @@ function config-path() {
 	echo "$( cd -P "$( dirname "$source" )" && pwd )" #dir has script loc'n
 }
 
+function is-file() {
+	if [ -e $1 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function is-link() {
+	if [ -L $1 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function is-dir() {
+	if [ -d $1 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
 
 # main execution after reading in all functions - for forward declaration
+main
